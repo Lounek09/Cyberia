8ba0e2ce6640497699c149f356c603a84df20b63
Lounek09 <lounek09@proton.me>
2022-11-09 11:20:25 +0100

    Add new TTG Lang in api

---
+++ Salamandra.Api/Datacenter/TTGCard.cs
@@ -0,0 +1,26 @@
+﻿namespace Salamandra.Api.Datacenter
+{
+    [Table("TTGCards")]
+    public sealed class TTGCard
+    {
+        [PrimaryKey, NotNull]
+        public int Id { get; set; }
+
+        [NotNull]
+        public int Index { get; set; }
+
+        [NotNull]
+        public int ItemId { get; set; }
+
+        [NotNull]
+        public int TTGEntityId { get; set; }
+
+        [NotNull]
+        public int Variant { get; set; }
+
+        public TTGCard()
+        {
+
+        }
+    }
+}
