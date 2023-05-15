0a90257a76261decda41203961e9db83e007b516
Lounek09 <lounek09@proton.me>
2023-05-15 12:30:29 +0200

    Correct effect 604 (spellLevel not spell)

--- Cyberia.Api/Factories/Effects/LearnSpellLevelEffect.cs
+++ Cyberia.Api/Factories/Effects/LearnSpellLevelEffect.cs
@@ -5,12 +5,12 @@ namespace Cyberia.Api.Factories.Effects
 {
     public sealed class LearnSpellLevelEffect : BasicEffect
     {
-        public int SpellId { get; init; }
+        public int SpellLevelId { get; init; }
 
         public LearnSpellLevelEffect(int effectId, EffectParameters parameters, int duration, int probability, string criteria, Area area) :
             base(effectId, parameters, duration, probability, criteria, area)
         {
-            SpellId = parameters.Param3;
+            SpellLevelId = parameters.Param3;
         }
 
         public static new LearnSpellLevelEffect Create(int effectId, EffectParameters parameters, int duration, int probability, string criteria, Area area)
@@ -18,16 +18,9 @@ namespace Cyberia.Api.Factories.Effects
             return new(effectId, parameters, duration, probability, criteria, area);
         }
 
-        public Spell? GetSpell()
-        {
-            return DofusApi.Instance.Datacenter.SpellsData.GetSpellById(SpellId);
-        }
-
         public override string GetDescription()
         {
-            string spellName = DofusApi.Instance.Datacenter.SpellsData.GetSpellNameById(SpellId);
-
-            return GetDescriptionFromParameters(null, null, spellName);
+            return GetDescriptionFromParameters(null, null, SpellLevelId.ToString());
         }
     }
 }
