3cbfc95e19511682a05b595f6d4ed59a80686cb1
Lounek09 <lounek09@proton.me>
2023-05-15 13:47:50 +0200

    Correct translation for PX criterion

--- Cyberia.Api/Factories/Criteria/PlayerCriteria/PlayerRightsCriterion.cs
+++ Cyberia.Api/Factories/Criteria/PlayerCriteria/PlayerRightsCriterion.cs
@@ -5,7 +5,7 @@
         public static string? GetValue(char @operator, string[] values)
         {
             if (values.Length > 0)
-                return $"Nom {@operator} {values[0].Bold()}";
+                return $"Niveau admin {@operator} {values[0].Bold()}";
 
             return null;
         }
