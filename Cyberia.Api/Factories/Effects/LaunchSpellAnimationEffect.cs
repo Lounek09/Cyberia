d12fcea93726b2afeee732724dd9e927151b23fb
Lounek09 <lounek09@proton.me>
2023-05-15 13:46:53 +0200

    Translate more effects

---
+++ Cyberia.Api/Factories/Effects/LaunchSpellAnimationEffect.cs
@@ -0,0 +1,26 @@
+﻿using Cyberia.Api.DatacenterNS;
+using Cyberia.Api.Managers;
+
+namespace Cyberia.Api.Factories.Effects
+{
+    public sealed class LaunchSpellLevelAnimationEffect : BasicEffect
+    {
+        public int SpellLevelId { get; init; }
+
+        public LaunchSpellLevelAnimationEffect(int effectId, EffectParameters parameters, int duration, int probability, string criteria, Area area) :
+            base(effectId, parameters, duration, probability, criteria, area)
+        {
+            SpellLevelId = parameters.Param1;
+        }
+
+        public static new LaunchSpellLevelAnimationEffect Create(int effectId, EffectParameters parameters, int duration, int probability, string criteria, Area area)
+        {
+            return new(effectId, parameters, duration, probability, criteria, area);
+        }
+
+        public override string GetDescription()
+        {
+            return GetDescriptionFromParameters(null, null, SpellLevelId.ToString());
+        }
+    }
+}
---
+++ Cyberia.Api/Factories/Effects/LaunchSpellLevelEffect.cs
@@ -0,0 +1,26 @@
+﻿using Cyberia.Api.DatacenterNS;
+using Cyberia.Api.Managers;
+
+namespace Cyberia.Api.Factories.Effects
+{
+    public sealed class LaunchSpellLevelEffect : BasicEffect
+    {
+        public int SpellLevelId { get; init; }
+
+        public LaunchSpellLevelEffect(int effectId, EffectParameters parameters, int duration, int probability, string criteria, Area area) :
+            base(effectId, parameters, duration, probability, criteria, area)
+        {
+            SpellLevelId = parameters.Param3;
+        }
+
+        public static new LaunchSpellLevelEffect Create(int effectId, EffectParameters parameters, int duration, int probability, string criteria, Area area)
+        {
+            return new(effectId, parameters, duration, probability, criteria, area);
+        }
+
+        public override string GetDescription()
+        {
+            return GetDescriptionFromParameters(null, null, SpellLevelId.ToString());
+        }
+    }
+}
