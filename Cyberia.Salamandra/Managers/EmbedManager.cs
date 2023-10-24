661fd99aab63d92d9c38e76181620cd82a074b9f
Samuel Hollard <s.hollard@lecolededesign.com>
2023-10-24 11:33:30 +0200

    Bypass Discord cache

--- Cyberia.Salamandra/Managers/EmbedManager.cs
+++ Cyberia.Salamandra/Managers/EmbedManager.cs
@@ -10,7 +10,7 @@ namespace Cyberia.Salamandra.Managers
         {
             DiscordEmbedBuilder embed = new DiscordEmbedBuilder()
                 .WithColor(new DiscordColor(Bot.Instance.Config.EmbedColor))
-                .WithFooter($"{Bot.Instance.Client.CurrentUser.Username} • {DateTime.Now.ToRolePlayString()} - {DateTime.Now:HH:mm}", $"{Bot.Instance.Api.Config.CdnUrl}/images/mini_salamandra.png");
+                .WithFooter($"{Bot.Instance.Client.CurrentUser.Username} • {DateTime.Now.ToRolePlayString()} - {DateTime.Now:HH:mm}", $"{Bot.Instance.Api.Config.CdnUrl}/images/mini-salamandra.png");
 
             string iconUrl = $"{Bot.Instance.Api.Config.CdnUrl}/images/embed_categories";
             switch (category)
