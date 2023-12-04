8ff796ae4459376b02e226c7da802d00cdb56a5e
Lounek09 <lounek09@proton.me>
2023-12-04 22:01:58 +0100

    Convert the dict to frozendict for the factories

--- Cyberia.Api/Factories/QuestObjectiveFactory.cs
+++ Cyberia.Api/Factories/QuestObjectiveFactory.cs
@@ -1,27 +1,30 @@
 ﻿using Cyberia.Api.Data.Quests;
 using Cyberia.Api.Factories.QuestObjectives;
 
+using System.Collections.Frozen;
+
 namespace Cyberia.Api.Factories;
 
 public static class QuestObjectiveFactory
 {
-    private static readonly Dictionary<int, Func<QuestObjectiveData, IQuestObjective?>> _factory = new()
-    {
-        { 0, FreeFormQuestObjective.Create },
-        { 1, GoToNpcQuestObjective.Create },
-        { 2, BringItemToNpcQuestObjective.Create },
-        { 3, BringItemToNpcQuestObjective.Create },
-        { 4, DiscoverMapQuestObjective.Create },
-        { 5, DiscoverMapSubAreaQuestObjective.Create },
-        { 6, MultiFightMonsterQuestObjective.Create },
-        { 7, FightMonsterQuestObjective.Create },
-        { 8, FreeFormQuestObjective.Create },
-        { 9, GoToNpcQuestObjective.Create },
-        { 10, BringItemToNpcQuestObjective.Create },
-        { 11, DuelSpecificPlayerQuestObjective.Create },
-        { 12, BringSoulToNpcQuestObjective.Create },
-        { 13, FightMonsterQuestObjective.Create }
-    };
+    private static readonly FrozenDictionary<int, Func<QuestObjectiveData, IQuestObjective?>> _factory =
+        new Dictionary<int, Func<QuestObjectiveData, IQuestObjective?>>()
+        {
+            { 0, FreeFormQuestObjective.Create },
+            { 1, GoToNpcQuestObjective.Create },
+            { 2, BringItemToNpcQuestObjective.Create },
+            { 3, BringItemToNpcQuestObjective.Create },
+            { 4, DiscoverMapQuestObjective.Create },
+            { 5, DiscoverMapSubAreaQuestObjective.Create },
+            { 6, MultiFightMonsterQuestObjective.Create },
+            { 7, FightMonsterQuestObjective.Create },
+            { 8, FreeFormQuestObjective.Create },
+            { 9, GoToNpcQuestObjective.Create },
+            { 10, BringItemToNpcQuestObjective.Create },
+            { 11, DuelSpecificPlayerQuestObjective.Create },
+            { 12, BringSoulToNpcQuestObjective.Create },
+            { 13, FightMonsterQuestObjective.Create }
+        }.ToFrozenDictionary();
 
     public static IQuestObjective GetQuestObjective(QuestObjectiveData questObjectiveData)
     {
