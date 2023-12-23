902df52e40a31157105d5c63355248db70a56ffa
Lounek09 <lounek09@proton.me>
2023-12-23 23:42:43 +0100

    Retrack TestCommandModule.cs...

---
+++ Cyberia.Salamandra/Commands/Admin/Test/TestCommandModule.cs
@@ -0,0 +1,27 @@
+﻿using Cyberia.Api;
+using Cyberia.Salamandra.Managers;
+
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
+
+using System.Text;
+
+namespace Cyberia.Salamandra.Commands.Admin;
+
+public sealed class TestCommandModule : ApplicationCommandModule
+{
+    [SlashCommand("test", "[Owner] Command to test random stuff")]
+    [SlashRequireOwner]
+    public async Task Command(InteractionContext _)
+    {
+        var builder = new StringBuilder();
+
+        builder.Append("Items:\n");
+        foreach (var itemStats in DofusApi.Datacenter.ItemsStatsData.ItemsStats)
+        {
+            await Task.Delay(0);
+
+            throw new NotImplementedException();
+        }
+    }
+}
