3bc5bafdb8318438ea64ad094664af0446676128
Lounek09 <lounek09@proton.me>
2023-12-23 22:20:35 +0100

    Partially trad effect 995

--- Cyberia.Api/Factories/Effects/Effects/RideDetailsEffect.cs
+++ Cyberia.Api/Factories/Effects/Effects/RideDetailsEffect.cs
@@ -4,16 +4,25 @@ using Cyberia.Api.Managers;
 
 namespace Cyberia.Api.Factories.Effects;
 
-public sealed record RideDetailsEffect : ParameterlessEffect, IEffect<RideDetailsEffect>
+public sealed record RideDetailsEffect : Effect, IEffect<RideDetailsEffect>
 {
-    private RideDetailsEffect(int effectId, int duration, int probability, CriteriaCollection criteria, EffectArea effectArea)
+    public int ItemUuid { get; init; }
+    public long UnknownId { get; init; } //Fuck long
+
+    private RideDetailsEffect(int effectId, int duration, int probability, CriteriaCollection criteria, EffectArea effectArea, int itemUuid, long unknownId)
         : base(effectId, duration, probability, criteria, effectArea)
     {
-
+        ItemUuid = itemUuid;
+        UnknownId = unknownId;
     }
 
     public static RideDetailsEffect Create(int effectId, EffectParameters parameters, int duration, int probability, CriteriaCollection criteria, EffectArea effectArea)
     {
-        return new(effectId, duration, probability, criteria, effectArea);
+        return new(effectId, duration, probability, criteria, effectArea, parameters.Param1, parameters.Param2);
+    }
+
+    public Description GetDescription()
+    {
+        return GetDescription(ItemUuid, UnknownId);
     }
 }
