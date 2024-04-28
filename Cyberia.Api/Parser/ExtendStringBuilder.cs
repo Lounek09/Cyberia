28245cbedadcaec3a6ce11ef07c895bec2b1c9ba
Lounek09 <lounek09@proton.me>
2024-04-29 00:11:54 +0200

    Update the lib

---
+++ Cyberia.Api/Parser/ExtendStringBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Cyberia.Api.Parser;
+
+public static class ExtendStringBuilder
+{
+    public static StringBuilder AppendJsonProperty(this StringBuilder builder, ReadOnlySpan<char> name, ReadOnlySpan<char> value, JsonValueKind kind)
+    {
+        builder.Append('"');
+        builder.Append(name);
+        builder.Append("\":");
+
+        if (kind == JsonValueKind.String)
+        {
+            builder.Append('"');
+        }
+
+        builder.Append(value);
+
+        if (kind == JsonValueKind.String)
+        {
+            builder.Append('"');
+        }
+
+        return builder;
+    }
+}
