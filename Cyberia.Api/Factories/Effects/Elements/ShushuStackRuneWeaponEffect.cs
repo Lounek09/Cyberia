58412ad571aafe7b2ff09fb580fd6c031fd4e25e
Lounek09 <lounek09@proton.me>
2024-05-15 23:05:51 +0200

    Wrong position of parameter in effect 740

--- Cyberia.Api/Factories/Effects/Elements/ShushuStackRuneWeaponEffect.cs
+++ Cyberia.Api/Factories/Effects/Elements/ShushuStackRuneWeaponEffect.cs
@@ -20,6 +20,6 @@ public sealed record ShushuStackRuneWeaponEffect : Effect, IEffect
 
     public Description GetDescription()
     {
-        return GetDescription(Amont);
+        return GetDescription(string.Empty, string.Empty, Amont);
     }
 }
