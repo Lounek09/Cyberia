fc2d4bd598b05b37d84df966f5002707e1c3f678
Lounek09 <lounek09@proton.me>
2024-06-21 01:12:57 +0200

    Add xml doc to EffectAreaFactory

--- Cyberia.Api/Factories/EffectAreaFactory.cs
+++ Cyberia.Api/Factories/EffectAreaFactory.cs
@@ -2,10 +2,21 @@
 
 namespace Cyberia.Api.Factories;
 
+/// <summary>
+/// Provides factory methods for creating <see cref="EffectArea"/>.
+/// </summary>
 public static class EffectAreaFactory
 {
+    /// <summary>
+    /// Gets the default <see cref="EffectArea"/>.
+    /// </summary>
     public static readonly EffectArea Default = new(80, 0);
 
+    /// <summary>
+    /// Creates an <see cref="EffectArea"/> from a compressed representation.
+    /// </summary>
+    /// <param name="compressedEffectArea">The compressed representation of the effect area.</param>
+    /// <returns>The created <see cref="EffectArea"/>.</returns>
     public static EffectArea Create(ReadOnlySpan<char> compressedEffectArea)
     {
         if (compressedEffectArea.Length != 2)
@@ -17,6 +28,11 @@ public static class EffectAreaFactory
         return new EffectArea(compressedEffectArea[0], PatternDecoder.CharToBase64Index(compressedEffectArea[1]));
     }
 
+    /// <summary>
+    /// Creates a list of <see cref="EffectArea"/> from a compressed representation.
+    /// </summary>
+    /// <param name="compressedEffectAreas">The compressed representation of multiple effect areas.</param>
+    /// <returns>The list of created <see cref="EffectArea"/>.</returns>
     public static List<EffectArea> CreateMany(ReadOnlySpan<char> compressedEffectAreas)
     {
         List<EffectArea> effectAreas = new(compressedEffectAreas.Length / 2);
