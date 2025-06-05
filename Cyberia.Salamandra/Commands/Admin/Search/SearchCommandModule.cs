b60ed8f121b11361af5e879f30cab61130160a51
Lounek09 <lounek09@proton.me>
2025-06-05 10:31:19 +0200

    Add a command to search the usage of an item's sprite

--- Cyberia.Salamandra/Commands/Admin/Search/SearchCommandModule.cs
+++ Cyberia.Salamandra/Commands/Admin/Search/SearchCommandModule.cs
@@ -1,4 +1,5 @@
 ﻿using Cyberia.Api.Data;
+using Cyberia.Api.Utils;
 using Cyberia.Salamandra.Enums;
 using Cyberia.Salamandra.Services;
 
@@ -72,7 +73,7 @@ public sealed class SearchCommandModule
 
         var embed = _embedBuilderService.CreateEmbedBuilder(EmbedCategory.Tools, "Tools", culture)
             .WithTitle($"Search effect {effectId} in {location}")
-            .WithDescription(descriptionBuilder.ToString().WithMaxLength(4000));
+            .WithDescription(descriptionBuilder.ToString().WithMaxLength(Constant.MaxEmbedDescriptionSize));
 
         await ctx.RespondAsync(embed);
     }
@@ -117,7 +118,43 @@ public sealed class SearchCommandModule
                 return;
         }
 
-        await ctx.RespondAsync(_embedBuilderService.CreateEmbedBuilder(EmbedCategory.Tools, $"Search criterion {criterionId} in {location}", culture)
-            .WithDescription(descriptionBuilder.ToString().WithMaxLength(4000)));
+        var embed = _embedBuilderService.CreateEmbedBuilder(EmbedCategory.Tools, "Tools", culture)
+            .WithTitle($"Search criterion {criterionId} in {location}")
+            .WithDescription(descriptionBuilder.ToString().WithMaxLength(Constant.MaxEmbedDescriptionSize));
+
+        await ctx.RespondAsync(embed);
+    }
+
+    [Command("item_sprite"), Description("Search where the item sprite is used")]
+    [SlashCommandTypes(DiscordApplicationCommandType.SlashCommand)]
+    public async Task ItemSpriteExecuteAsync(SlashCommandContext ctx,
+        [Parameter("item_type_id"), Description("Item type ID")]
+        [MinMaxValue(0, 999)]
+        int itemTypeId,
+        [Parameter("gfx_id"), Description("Gfx ID")]
+        [MinMaxValue(-1, 999_999)]
+        int gfxId)
+    {
+        var culture = await _cultureService.GetCultureAsync(ctx.Interaction);
+
+        StringBuilder descriptionBuilder = new();
+        foreach (var itemData in _dofusDatacenter.ItemsRepository.Items.Values)
+        {
+            if (itemData.ItemTypeId == itemTypeId && itemData.GfxId == gfxId)
+            {
+                descriptionBuilder.Append("- ");
+                descriptionBuilder.Append(itemData.Name.ToString(culture));
+                descriptionBuilder.Append(" (");
+                descriptionBuilder.Append(itemData.Id);
+                descriptionBuilder.Append(")\n");
+            }
+        }
+
+        var embed = _embedBuilderService.CreateEmbedBuilder(EmbedCategory.Tools, "Tools", culture)
+            .WithTitle($"Search gfx {gfxId} in {_dofusDatacenter.ItemsRepository.GetItemTypeNameById(itemTypeId, culture)} ({itemTypeId})")
+            .WithThumbnail(await ImageUrlProvider.GetImagePathAsync($"items/{itemTypeId}", gfxId, CdnImageSize.Size128))
+            .WithDescription(descriptionBuilder.ToString().WithMaxLength(Constant.MaxEmbedDescriptionSize));
+
+        await ctx.RespondAsync(embed);
     }
 }
