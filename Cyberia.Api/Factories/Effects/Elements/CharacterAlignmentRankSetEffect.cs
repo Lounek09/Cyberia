f6863a00030849365033cddfe3331dee42da33dc
Lounek09 <lounek09@proton.me>
2025-09-03 21:44:12 +0200

    Add and update some effects

---
+++ Cyberia.Api/Factories/Effects/Elements/CharacterAlignmentRankSetEffect.cs
@@ -0,0 +1,27 @@
+﻿using Cyberia.Api.Factories.Criteria.Elements;
+using Cyberia.Api.Factories.EffectAreas;
+
+using System.Globalization;
+
+namespace Cyberia.Api.Factories.Effects.Elements;
+
+public sealed record CharacterAlignmentRankSetEffect : Effect
+{
+    public int Rank { get; init; }
+
+    private CharacterAlignmentRankSetEffect(int id, int duration, int probability, CriteriaReadOnlyCollection criteria, bool dispellable, EffectArea effectArea, int rank)
+        : base(id, duration, probability, criteria, dispellable, effectArea)
+    {
+        Rank = rank;
+    }
+
+    internal static CharacterAlignmentRankSetEffect Create(int effectId, EffectParameters parameters, int duration, int probability, CriteriaReadOnlyCollection criteria, bool dispellable, EffectArea effectArea)
+    {
+        return new(effectId, duration, probability, criteria, dispellable, effectArea, (int)parameters.Param1);
+    }
+
+    public override DescriptionString GetDescription(CultureInfo? culture = null)
+    {
+        return GetDescription(culture, Rank);
+    }
+}
--- Cyberia.Api/Factories/Effects/Elements/CharacterAlignmentSideSetEffect.cs
+++ Cyberia.Api/Factories/Effects/Elements/CharacterAlignmentSideSetEffect.cs
@@ -1,19 +1,47 @@
-﻿using Cyberia.Api.Factories.Criteria.Elements;
+﻿using Cyberia.Api.Data.Alignments;
+using Cyberia.Api.Factories.Criteria.Elements;
 using Cyberia.Api.Factories.EffectAreas;
-using Cyberia.Api.Factories.Effects.Templates;
+
+using System.Globalization;
 
 namespace Cyberia.Api.Factories.Effects.Elements;
 
-public sealed record CharacterAlignmentSideSetEffect : ParameterlessEffect
+public sealed record CharacterAlignmentSideSetEffect : Effect
 {
-    private CharacterAlignmentSideSetEffect(int id, int duration, int probability, CriteriaReadOnlyCollection criteria, bool dispellable, EffectArea effectArea)
+    public int AlignmentId { get; init; }
+
+    public int AlignmentSpecializationId { get; init; }
+
+    public int Level { get; init; }
+
+    private CharacterAlignmentSideSetEffect(int id, int duration, int probability, CriteriaReadOnlyCollection criteria, bool dispellable, EffectArea effectArea, int alignmentId, int alignmentSpecializationId, int level)
         : base(id, duration, probability, criteria, dispellable, effectArea)
     {
-
+        AlignmentId = alignmentId;
+        AlignmentSpecializationId = alignmentSpecializationId;
+        Level = level;
     }
 
-    internal static CharacterAlignmentSideSetEffect Create(int effectId, EffectParameters _, int duration, int probability, CriteriaReadOnlyCollection criteria, bool dispellable, EffectArea effectArea)
+    internal static CharacterAlignmentSideSetEffect Create(int effectId, EffectParameters parameters, int duration, int probability, CriteriaReadOnlyCollection criteria, bool dispellable, EffectArea effectArea)
     {
-        return new(effectId, duration, probability, criteria, dispellable, effectArea);
+        return new(effectId, duration, probability, criteria, dispellable, effectArea, (int)parameters.Param1, (int)parameters.Param2, (int)parameters.Param3);
+    }
+
+    public AlignmentData? GetAlignmentData()
+    {
+        return DofusApi.Datacenter.AlignmentsRepository.GetAlignmentDataById(AlignmentId);
+    }
+
+    public AlignmentSpecializationData? GetAlignmentSpecializationData()
+    {
+        return DofusApi.Datacenter.AlignmentsRepository.GetAlignmentSpecializationDataById(AlignmentSpecializationId);
+    }
+
+    public override DescriptionString GetDescription(CultureInfo? culture = null)
+    {
+        var alignmentName = DofusApi.Datacenter.AlignmentsRepository.GetAlignmentNameById(AlignmentId, culture);
+        var alignmentSpecializationName = DofusApi.Datacenter.AlignmentsRepository.GetAlignmentSpecializationNameById(AlignmentSpecializationId, culture);
+
+        return GetDescription(culture, alignmentName, alignmentSpecializationName, Level);
     }
 }
