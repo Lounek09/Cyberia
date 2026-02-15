6c259e503cd44308d81a385e9bc4258b48e2aa78
Lounek09 <lounek09@proton.me>
2026-02-15 19:32:08 +0100

    Add extention utility methods to Lang and LangRepository

---
+++ Cyberia.Langzilla/Extensions/LangExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Cyberia.Database.Models;
+using Cyberia.Database.Repositories;
+using Cyberia.Langzilla.Enums;
+
+namespace Cyberia.Langzilla.Extensions;
+
+/// <summary>
+/// Provides extension methods for the <see cref="Lang"/> class.
+/// </summary>
+public static class LangExtensions
+{
+    extension(Lang lang)
+    {
+        /// <summary>
+        /// Gets the name of the swf file.
+        /// </summary>
+        public string FileName => $"{lang.Name}_{lang.Language.ToStringFast()}_{lang.Version}.swf";
+
+        /// <summary>
+        /// Gets the route of the swf file.
+        /// </summary>
+        public string Route => $"{LangsWatcher.GetRoute(lang.Type)}/swf/{lang.FileName}";
+
+        /// <summary>
+        /// Gets the directory output path of the lang.
+        /// </summary>
+        public string OutputPath => Path.Join(LangRepository.GetOutputPath(new LangsIdentifier(lang.Type, lang.Language)), lang.Name);
+
+        /// <summary>
+        /// Gets the file path of the lang.
+        /// </summary>
+        public string FilePath => Path.Join(lang.OutputPath, lang.FileName);
+
+        /// <summary>
+        /// Gets the generated decompiled file path of the lang.
+        /// </summary>
+        public string DecompiledFilePath => Path.Join(lang.OutputPath, "current.as");
+
+        /// <summary>
+        /// Gets the generated diff file path of the lang.
+        /// </summary>
+        public string DiffFilePath => Path.Join(lang.OutputPath, "diff.as");
+    }
+}
---
+++ Cyberia.Langzilla/Extensions/LangRepositoryExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Cyberia.Database.Repositories;
+using Cyberia.Langzilla.Enums;
+
+namespace Cyberia.Langzilla.Extensions;
+
+public static class LangRepositoryExtensions
+{
+    extension(LangRepository repository)
+    {
+        /// <summary>
+        /// Gets the name of the versions file.
+        /// </summary>
+        /// <param name="language">The language of the versions file.</param>
+        /// <returns>The name of the versions file.</returns>
+        public static string GetVersionsFileName(Language language)
+        {
+            return $"versions_{language.ToStringFast()}.txt";
+        }
+
+        /// <summary>
+        /// Gets the route to the versions file.
+        /// </summary>
+        /// <param name="identifier">The identifier of the versions file.</param>
+        /// <returns>The route to the versions file.</returns>
+        public static string GetVersionsFileRoute(LangsIdentifier identifier)
+        {
+            return $"{LangsWatcher.GetRoute(identifier.Type)}/{GetVersionsFileName(identifier.Language)}";
+        }
+
+        /// <summary>
+        /// Gets the output path.
+        /// </summary>
+        /// <param name="identifier">The identifier of the output path.</param>
+        /// <returns>The output path.</returns>
+        public static string GetOutputPath(LangsIdentifier identifier)
+        {
+            return Path.Join(LangsWatcher.OutputPath, identifier.Type.ToStringFast(), identifier.Language.ToStringFast());
+        }
+
+        /// <summary>
+        /// Gets the path to the versions file.
+        /// </summary>
+        /// <param name="identifier">The identifier of the versions file.</param>
+        /// <returns>The path to the versions file.</returns>
+        public static string GetVersionsFilePath(LangsIdentifier identifier)
+        {
+            return Path.Join(GetOutputPath(identifier), GetVersionsFileName(identifier.Language));
+        }
+    }
+}
