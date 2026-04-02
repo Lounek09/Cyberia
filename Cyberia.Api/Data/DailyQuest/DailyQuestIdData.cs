e3442a9cf494e2488c5c5f8d6749fdebf78acde2
Lounek09 <lounek09@proton.me>
2026-04-02 15:37:17 +0200

    Add new DQI data from DailyQuest lang

---
+++ Cyberia.Api/Data/DailyQuest/DailyQuestIdData.cs
@@ -0,0 +1,22 @@
+﻿using Cyberia.Api.Data.Quests;
+
+using System.Text.Json.Serialization;
+
+namespace Cyberia.Api.Data.DailyQuest;
+
+public sealed class DailyQuestIdData : IDofusData<int>
+{
+    [JsonPropertyName("id")]
+    public int Id { get; init; }
+
+    [JsonPropertyName("v")]
+    public int QuestId { get; init; }
+
+    [JsonConstructor]
+    internal DailyQuestIdData() { }
+
+    public QuestData? GetQuestData()
+    {
+        return DofusApi.Datacenter.QuestsRepository.GetQuestDataById(QuestId);
+    }
+}
--- Cyberia.Api/Data/DailyQuest/DailyQuestRepository.cs
+++ Cyberia.Api/Data/DailyQuest/DailyQuestRepository.cs
@@ -13,15 +13,26 @@ public sealed class DailyQuestRepository : DofusRepository, IDofusRepository
     [JsonConverter(typeof(DofusDataFrozenDictionaryConverter<int, DailyQuestData>))]
     public FrozenDictionary<int, DailyQuestData> DailyQuests { get; init; }
 
+    [JsonPropertyName("DQI")]
+    [JsonConverter(typeof(DofusDataFrozenDictionaryConverter<int, DailyQuestData>))]
+    public FrozenDictionary<int, DailyQuestIdData> DailyQuestIds { get; init; }
+
     [JsonConstructor]
     internal DailyQuestRepository()
     {
         DailyQuests = FrozenDictionary<int, DailyQuestData>.Empty;
+        DailyQuestIds = FrozenDictionary<int, DailyQuestIdData>.Empty;
     }
 
     public DailyQuestData? GetDailyQuestDataById(int id)
     {
         DailyQuests.TryGetValue(id, out var dailyQuestData);
         return dailyQuestData;
+    }
+
+    public DailyQuestIdData? GetDailyQuestIdDataById(int id)
+    {
+        DailyQuestIds.TryGetValue(id, out var dailyQuestIdData);
+        return dailyQuestIdData;
     }
 }
