03249800e65dc4949f626eaacd2d2b2b4b48494e
Lounek09 <lounek09@proton.me>
2026-06-10 17:11:06 +0200

    Remove tools temporary to readd it as Tools

--- tools/CsvGenerator/Generators/ICsvGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CsvGenerator.Generators;
-
-public interface ICsvGenerator
-{
-    string Name { get; }
-    string FilePath { get; }
-
-    void Generate();
-}
--- tools/CsvGenerator/README.md
+++
@@ -1,3 +0,0 @@
-# CsvGenerator
-
-A tool to generate CSV files from the dofus langs data generated using [Cyberia.Langzilla](../Cyberia.Langzilla/).
