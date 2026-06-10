f81c9d157aa71296ed84c7cae38343f28d4bba10
Lounek09 <lounek09@proton.me>
2026-06-10 17:04:11 +0200

    Remove github action and create a script to deploy the app to the server

---
+++ Scripts/Deploy.cs
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env dotnet run
+
+#:include Shared/*.cs
+
+const string ProjectName = "Cyberia";
+const string ProjectPath = $"../{ProjectName}/{ProjectName}.csproj";
+const string RemoteUser = "salamandra";
+const string RemoteHost = "amphibian.fr";
+const int RemotePort = 9;
+
+const string OutputPath = $"../publish/{ProjectName}";
+const string RemotePath = $"/var/www/{ProjectName.ToLowerInvariant()}/App";
+
+Command.Execute("dotnet", $"publish \"{ProjectPath}\" -c Release -r linux-x64 -o \"{OutputPath}\" -p:PublishReadyToRun=true -p:DebugType=Embedded");
+Command.Execute("scp", $"-r -P {RemotePort} {OutputPath}/* {RemoteUser}@{RemoteHost}:{RemotePath}");
+Command.Execute("ssh", $"{RemoteUser}@{RemoteHost} -p {RemotePort} \"chmod -R a+x {RemotePath}/flare\"");
---
+++ Scripts/Shared/Command.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+public static class Command
+{
+    public static void Execute(string command, string args)
+    {
+        Console.WriteLine($"> {command} {args}");
+
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = command,
+            Arguments = args,
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        };
+
+        using var process = new Process
+        {
+            StartInfo = startInfo
+        };
+
+        process.OutputDataReceived += (_, e) =>
+        {
+            Console.WriteLine(e.Data);
+        };
+
+        process.ErrorDataReceived += (_, e) =>
+        {
+            Console.Error.WriteLine(e.Data);
+        };
+
+        process.Start();
+
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            throw new Exception($"Command failed with exit code {process.ExitCode}");
+        }
+    }
+}
